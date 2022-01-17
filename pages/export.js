6d7d1537a424009d89b46a3f7acbc9d4af02f7fb
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-01-17 16:18:09 -0300

    corrigindo exportação

--- pages/export.js
+++ pages/export.js
@@ -1,4 +1,14 @@
-export function Anime(){
+export default function Anime1(){
+  return (
+     <div>
+        <h2>
+          Boruto!
+        </h2>
+     </div>
+  )
+}
+
+export function Anime2(){
   return (
     <div>
        <h1>
